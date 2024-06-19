a574523e3c9160914f8f3669eb07a0dff84fff00
Ging Louie <ging.m.louie@gmail.com>
2024-06-19 10:30:05 -0700

    Style changes

--- script.js
+++ script.js
@@ -6,6 +6,21 @@ document
 
 const parsedRecords = []; // Declare the array globally
 
+var coll = document.getElementsByClassName('collapsible');
+var i;
+
+for (i = 0; i < coll.length; i++) {
+	coll[i].addEventListener('click', function () {
+		this.classList.toggle('active');
+		var content = this.nextElementSibling;
+		if (content.style.display === 'block') {
+			content.style.display = 'none';
+		} else {
+			content.style.display = 'block';
+		}
+	});
+}
+
 function handleFileSelect(event) {
 	const file = event.target.files[0];
 	if (!file) return;
